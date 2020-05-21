ee257db21f7014198e3338f08605ede0a9cdfe94
Tássio <tassio72@hotmail.com>
2020-05-21 16:03:21 -0300

    feat/add-jsm-components

---
+++ src/custom.d.ts
@@ -0,0 +1,1 @@
+declare module 'jsm-components'
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import Vue from 'vue'
+import * as JsmComponents from 'jsm-components'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(JsmComponents.JsmButton)
+Vue.use(JsmComponents.JsmInput)
+Vue.use(JsmComponents.JsmCard)
+Vue.use(JsmComponents.JsmBreadcrumb)
+Vue.use(JsmComponents.JsmCheckbox)
+Vue.use(JsmComponents.JsmRecaptcha)
+Vue.use(JsmComponents.JsmSelect)
+Vue.use(JsmComponents.JsmTable)
+Vue.use(JsmComponents.JsmLoading)
+Vue.use(JsmComponents.JsmModal)
+Vue.use(JsmComponents.JsmNotification)
+Vue.use(JsmComponents.JsmMetter)
+Vue.use(JsmComponents.JsmAccordion)
 
 new Vue({
   router,
