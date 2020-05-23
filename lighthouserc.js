8a5c4b2b575327a4c7ad53073eeb4926db10458b
Tássio <tassio72@hotmail.com>
2020-05-23 17:20:23 -0300

    test: LHCI add

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,7 @@ module.exports = {
     assert: {
       assertions: {
         // "categories:performance": ["error", { "minScore": 0.8 }],
-        // "first-contentful-paint": ["error", { "maxNumericValue": 2000 }],
+        "first-contentful-paint": ["error", { "maxNumericValue": 2000 }],
         // "interactive": ["error", { "maxNumericValue": 5200 }],
         // "speed-index": ["error", { "maxNumericValue": 4300 }],
         // "resource-summary:script:size": ["error", { "maxNumericValue": 170000 }],
