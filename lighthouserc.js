209d317c76edbabc718db6417c65d0c7ca3dc575
Tássio <tassio72@hotmail.com>
2020-05-23 16:42:05 -0300

    test: LHCI add

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,5 +3,21 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
-  },
-};
+    "collect": {
+      "url": [
+        "http://localhost:3000/",
+        "http://localhost:3000/hello-vue"
+      ],
+      "numberOfRuns": 3
+    },
+    "assert": {
+      "assertions": {
+        "categories:performance": ["error", { "minScore": 0.8 }],
+        "first-contentful-paint": ["error", { "maxNumericValue": 2000 }],
+        "interactive": ["error", { "maxNumericValue": 5200 }],
+        "speed-index": ["error", { "maxNumericValue": 4300 }],
+        "resource-summary:script:size": ["error", { "maxNumericValue": 170000 }],
+        "resource-summary:total:size": ["error", { "maxNumericValue": 300000 }]
+      }
+    },
+  };
